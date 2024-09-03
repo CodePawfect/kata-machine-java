45ba049a1d8c0404c3e9ed62dfdc62a008daf329
codepawfect <codepawfect@gmail.com>
2024-09-03 22:09:10 +0200

    improved BFS Graph algorithm

--- src/main/java/BFSGraph.java
+++ src/main/java/BFSGraph.java
@@ -1,42 +1,32 @@
 import model.Graph;
 
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Set;
 
 public class BFSGraph {
 
     public Graph.Vertex breadthFirstSearch(Graph graph, char needle) {
+        int[][] adjacencyMatrix = graph.getAdjacencyMatrix();
+        boolean[] seen = new boolean[adjacencyMatrix.length];
         Queue<Graph.Vertex> queue = new LinkedList<>();
-        int curVertexIndex = 0;
+        int vertexIndex = 0;
 
-        Set<Graph.Vertex> traversed = new HashSet<>();
-        queue.add(graph.getVertex(curVertexIndex));
+        queue.add(graph.getVertex(0));
 
         while(!queue.isEmpty()) {
-           var vertex = queue.poll();
-           addNextVerticesToQueue(curVertexIndex, queue, graph);
+            Graph.Vertex curVertex = queue.poll();
 
-           if(traversed.contains(vertex)) continue;
-           if(vertex.value() == needle) return vertex;
+            if(curVertex.value() == needle) return curVertex;
+            seen[vertexIndex++] = true;
 
-           traversed.add(vertex);
-           curVertexIndex++;
+            for(int i = 0; i < adjacencyMatrix.length; i++) {
+                if(adjacencyMatrix[vertexIndex][i] == 1 && !seen[i]) {
+                    queue.add(graph.getVertex(i));
+                }
+            }
         }
 
         return null;
-    }
-
-    private void addNextVerticesToQueue(int vertexIndex, Queue<Graph.Vertex> queue, Graph graph) {
-        int[][] adjacencyMatrix = graph.getAdjacencyMatrix();
-
-        for(int j = 0; j < adjacencyMatrix.length; j++) {
-            if(adjacencyMatrix[vertexIndex][j] == 1) {
-                Graph.Vertex connectedVertex = graph.getVertex(j);
-                queue.add(connectedVertex);
-            }
-        }
     }
 
 }
